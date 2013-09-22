0926650710da7b66e9519cdea33fb49f6edacf2e
Wu Yinghao <wyh817@gmail.com>
2013-09-22 17:00:35 +0800

    modify parseRegs
    完成，分析.v文件，生成寄存器列表说明的xml供vc使用

--- Src/pythonPRJ/parseRegs.py
+++ Src/pythonPRJ/parseRegs.py
@@ -17,9 +17,17 @@ def readFiles():
         if(len(line)>0):
             #print line
             useful_lines.append(line)
+    f.close()
     return useful_lines
 
-
+def writeFiles(input):
+    f=open("/Users/wuyinghao/Documents/allreg_read.xml","w")
+    f.write("<?xml version=\"1.0\" encoding=\"GB2312\"?>\r\n")
+    for i in input:
+        f.write(i)
+        f.write("\r\n")
+    f.close()
+    
 #
 #格式化，分组
 #
@@ -141,45 +149,155 @@ def parseAddrs(gruops):
 
 def parseAddrToXML(gruop):
     
-    print gruop[0]
-    print gruop[1]
-    print "++++++++++++++++++++++++"
-    start_reg=0;
-    start_addr=0;
-    addr=0
-    addr_and_regs=[]
-    addr_and_regs.append(gruop[1][addr])
-    for reg in gruop[0]:
-        if(reg[4]<gruop[1][addr][2]):
-            #print str(reg)+"++++"
-            addr_and_regs.append(reg)
-            start_addr=start_addr+reg[2]
+    #print gruop[0]
+    #print gruop[1]
+    #print "++++++++++++++++++++++++"
+    addrs=gruop[1]
+    regs=gruop[0]
+    addrs_lens=[]
+    regs_lens=[]
+    addrslen=0
+    regslen=0
+    for addr in  gruop[1]:
+        addrs_lens.append(addr[2])
+        addrslen=addrslen+addr[2]
+        
+    for reg in regs:
+        regs_lens.append(reg[2])
+        regslen=regslen+reg[2]
+        #print reg
+        
+    regstart=0
+    addrstart=0
+    pos=0
+    reglist=len(regs_lens)
+    count=0
+    
+    #print addrs_lens
+    #print regs_lens
+    
+    res=[]
+    
+    while(count < reglist ):
+        if(regs_lens[count]-regstart < addrs_lens[pos]-addrstart):
+            #print str(regs_lens[count]) + " in ADDR " + str(pos)
+            #print "ADDR : [ " + str(gruop[1][pos][1]) + " : " + str(gruop[1][pos][2]) + "] ::::" + \
+            #" REG: [" + str(gruop[0][count][1]) + " : " +str(gruop[0][count][2]) + " ]" + \
+            #" START : " + str(addrstart) + \
+            #" END : "  + str(addrstart+regs_lens[count]-regstart-1)
+            res.append([gruop[1][pos][1],gruop[1][pos][2],gruop[0][count][1],gruop[0][count][2],addrstart,addrstart+regs_lens[count]-regstart-1,0])
+            
+            #regstart+regs_lens[count]
+            addrstart=addrstart+regs_lens[count]-regstart
+            regstart=0
+            count=count+1
         else:
-            addr_and_regs.append(reg)
-            addr=addr+1
-            addr_and_regs.append(gruop[1][addr])
-            addr_and_regs.append(reg)
-    print addr_and_regs
-    print "========================="
+            if(regs_lens[count]-regstart > addrs_lens[pos]-addrstart):
+                #print " FK " + str(regs_lens[count]) + " in ADDR " + str(pos)
+                #print "ADDR : [ " + str(gruop[1][pos][1]) + " : " + str(gruop[1][pos][2]) + "] ::::" + \
+                #" REG: [" + str(gruop[0][count][1]) + " : " +str(gruop[0][count][2]) + " ]" + \
+                #" START : " + str(addrstart) + \
+                #" END : "  + str(addrstart+addrs_lens[pos]-addrstart-1)
+                res.append([gruop[1][pos][1],gruop[1][pos][2],gruop[0][count][1],gruop[0][count][2],addrstart,addrstart+regs_lens[count]-regstart-1,0])
+                
+                regstart=regstart+addrs_lens[pos]-addrstart
+                addrstart=0
+                pos=pos+1
+            else :
+                if (regs_lens[count]-regstart == addrs_lens[pos]-addrstart):
+                    #print " ZH " + str(regs_lens[count]) + " in ADDR " + str(pos)
+                    #print "ADDR : [ " + str(gruop[1][pos][1]) + " : " + str(gruop[1][pos][2]) + "] ::::" + \
+                    #" REG: [" + str(gruop[0][count][1]) + " : " +str(gruop[0][count][2]) + " ]" + \
+                    #" START : " + str(addrstart) + \
+                    #" END : "  + str(addrstart+regs_lens[count]-regstart-1)
+                    res.append([gruop[1][pos][1],gruop[1][pos][2],gruop[0][count][1],gruop[0][count][2],addrstart,addrstart+regs_lens[count]-regstart-1,0])
+                    
+                    addrstart=0
+                    regstart=0
+                    pos=pos+1
+                    count=count+1
+        #print "count : " + str(count)
+        #print "addrstart : " + str(addrstart)
+    return res
+        
+    
+        
+    
             
     
     
 
 def parseAddrsToXML(gruops):
+    res=[]
     for gruop in gruops:
-        parseAddrToXML(gruop)
-
+        r=parseAddrToXML(gruop)
+        for k in r:
+            res.append(k)
+    
+    return res
+    
+    
+    
+def parCreateXML(input):
+    head=input[0][0]
+    print head
+    addr=[]
+    res=[]
+    for val in input:
+        if val[0] == head :
+            addr.append(val)
+        else:
+            head=val[0]
+            res.append(addr)
+            addr=[]
+            addr.append(val)
+    return res
+    
+    
+xmlres=[]
+    
+def createXLMNode(node):
+    
+    xmlres.append("\t\t<name>" + node[0][0] + "</name>")
+    xmlres.append( "\t\t<addr>" + node[0][0] + "</addr>")
+    xmlres.append( "\t\t<len>" + str(node[0][1]) + "</len>")
+    i=1
+    for val in node :
+        #print val
+        xmlres.append( "\t\t<part" + str(i) +">")
+        xmlres.append( "\t\t\t<name>" + val[2] + "</name>")
+        xmlres.append( "\t\t\t<addr>" + val[0] + "</addr>")
+        xmlres.append( "\t\t\t<start>" + str(val[4]) + "</start>")
+        xmlres.append( "\t\t\t<end>" + str(val[5]) + "</end>")
+        xmlres.append( "\t\t\t<data>" + str(val[6]) + "</data>")
+        xmlres.append( "\t\t</part" + str(i) +">")
+        i=i+1
+        
+        
+        
+        
+        
+    
+def createXML(input):
+    xmlres.append( "<register>")
+    for val in input:
+        xmlres.append( "\t<reg>")
+        createXLMNode(val)
+        xmlres.append( "\t</reg>")
+        xmlres.append( "")
+    xmlres.append( "</register>")
 
 lines=readFiles()
 gruops=gruopLines(lines)
 formatd=parseGruops(gruops)
 parseFormatedGruops(formatd)
 reg_addrs=parseAddrs(formatd)
-parseAddrsToXML(reg_addrs)
-"""
-for gruop in reg_addrs:
-    print gruop[0]
-    print "+++++++++"
-    print gruop[1]
-    print "========="
-"""
+res = parseAddrsToXML(reg_addrs)
+
+res = parCreateXML(res)
+
+createXML(res)
+writeFiles(xmlres)
+
+for i in xmlres:
+    print i
