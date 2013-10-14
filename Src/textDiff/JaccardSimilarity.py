5f6464307358c4bfdd639acd3f23fc64b506960c
Wu Yinghao <wyh817@gmail.com>
2013-10-14 15:40:49 +0800

    modify jaccardSimilarity.py
    修改，添加注释

--- Src/textDiff/JaccardSimilarity.py
+++ Src/textDiff/JaccardSimilarity.py
@@ -1,16 +1,25 @@
 # -*- coding=utf-8 -*-
 
-
+#
+#
+#
+#
 # 利用jaccard similarity 计算文本相似度
+#
+#
+#
+#
+
 
 import os
 
-
+#############################################
 #
 # 读取文件，保存到一个字符串中
 # 输入： 文件名完整路径
 # 输出： 文件内容
 #
+#############################################
 def readFile(file_name):
     f=open(file_name,"r")
     file_contents=f.read()
@@ -22,12 +31,13 @@ def readFile(file_name):
     
 
     
-
+#############################################
 #
 # 分割字符串，使用k-shingle方式进行分割
 # 输入：字符串，k值
 # 输出：分割好的字符串，存入数组中
 #
+#############################################
 def splitContents(content,k=5):
     content_split=[]
     for i in range(len(content)-k):
@@ -35,12 +45,13 @@ def splitContents(content,k=5):
     return content_split
     
 
-
+#############################################
 #
 # 将数据保存到hash表中，也就是某个集合
 # 输入：已经分隔好的数据
 # 输出：hash表
 #
+#############################################
 def hashContentsList(content_list):
     hash_content={}
     for i in content_list:
@@ -49,28 +60,15 @@ def hashContentsList(content_list):
         else:
             hash_content[i]=1
     return hash_content
-
-
-
-
-#
-# 从某个文本文件获取一个集合，该集合保存了文本中单词的出现频率
-# 输入：文件名，k值,默认为5
-# 输出：一个词频的hash表
-#
-def getHashInfoFromFile(file_name,k=5):
-    content=readFile(file_name)
-    content_list = splitContents(content,k)
-    hash_content = hashContentsList(content_list)
-    return hash_content
     
     
-    
+#############################################   
 #
 # 计算交集
 # 输入：两个hash表
 # 输出：交集的整数
 #
+#############################################
 def calcIntersection(hash_a,hash_b):
     intersection=0
     if(len(hash_a) <= len(hash_b)):
@@ -90,11 +88,13 @@ def calcIntersection(hash_a,hash_b):
     return intersection
 
 
+#############################################
 #
 # 计算并集
 # 输入：两个hash表
 # 输出：并集的整数
 #
+#############################################
 def calcUnionSet(hash_a,hash_b,intersection):
     union_set=0
     
@@ -106,9 +106,37 @@ def calcUnionSet(hash_a,hash_b,intersection):
     return union_set-intersection
     
     
-    
+#############################################
+#
+# 计算相似度
+# 输入：交集和并集
+# 输出：相似度
+#   
+#############################################
 def calcSimilarity(intersection,union_set):
     return float(intersection)/float(union_set)
+
+
+#############################################
+#
+# 从某个文本文件获取一个集合，该集合保存了文本中单词的出现频率
+# 输入：文件名，k值,默认为5
+# 输出：一个词频的hash表
+#
+#############################################
+def getHashInfoFromFile(file_name,k=5):
+    content=readFile(file_name)
+    content_list = splitContents(content,k)
+    hash_content = hashContentsList(content_list)
+    return hash_content
+
+
+#############################################
+#
+# 主程序
+#
+#
+#############################################
 
 hash_content1=getHashInfoFromFile("/Users/wuyinghao/Documents/test1.txt",5)
 hash_content2=getHashInfoFromFile("/Users/wuyinghao/Documents/test2.txt",5)
@@ -124,7 +152,6 @@ print union_set
 print "similarity is : " + str(calcSimilarity(intersection,union_set))
 
 #print len(hash_content)
-
 #content = readFile("/Users/wuyinghao/Documents/allreg_read.txt")
 #content_list = splitContents(content,8)
 #hash_content = hashContentsList(content_list)
