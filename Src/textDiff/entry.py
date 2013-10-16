17954137d1d6224cdcda9b16e2c1ff9043b54c3c
Wu Yinghao <wyh817@gmail.com>
2013-10-16 17:57:52 +0800

    modify entry.py
    添加一些对比信息

--- Src/textDiff/entry.py
+++ Src/textDiff/entry.py
@@ -14,7 +14,7 @@ import NewJaccardSimilarity
 import JaccardSimilarity
 import time
 
-file_path="/Users/wuyinghao/Desktop/test/data/sex/"
+file_path="/Users/wuyinghao/Desktop/test/data/media/"
 min_hash_args=[[1,1],[3,1],[5,7]]
 k=5
 
@@ -23,9 +23,8 @@ print u"使用基本JaccardSimilarity开始计算相似度...."
 start = time.time()
 
 res=JaccardSimilarity.calcSimilarityByWords(file_path,k)
-print u"显示输出...."
-for i in res[:10]:
-    print i
+res1=res[:20]
+
     
 
 end = time.time()
@@ -36,12 +35,33 @@ print u"使用优化JaccardSimilarity开始计算近似相似度...."
 start = time.time()
 
 res=NewJaccardSimilarity.calcSimilarityByMiniHashSignaturesMatrix(file_path,min_hash_args,k)
-
-print u"显示输出...."
-for i in res[:10]:
-    print i
+res2=res[:20]
 
 end = time.time()
 print u"使用优化JaccardSimilarity计算近似相似度结束，总共用时: " + str(end-start) + u"秒"
+print ""
+print ""
+print u"============================================================================="
+print u"============================================================================="
+print ""
+print ""
+
+print u"使用基本JaccardSimilarity计算结果，显示输出...."
+for i in res1:
+    print i
+
+
+print u"使用优化JaccardSimilarity计算近似相似度结果，显示输出...."
+for i in res2:
+    print i
+    
+catch=0.0
+for v in res2:
+    for k in res1:
+        if(v[1]==k[1] and v[2]==k[2]):
+            catch=catch+1
+
+print u"前20名命中率:::" + str(catch/20)
+
 
 
