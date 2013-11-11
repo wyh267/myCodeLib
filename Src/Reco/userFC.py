0bfbb7aff77faaa8aa6427f9da64fd79488d3f8a
Wu Yinghao <wyh817@gmail.com>
2013-11-11 23:39:53 +0800

    modify
    modify recommd

--- Src/Reco/userFC.py
+++ Src/Reco/userFC.py
@@ -4,6 +4,33 @@ import math
 import sys
 from texttable import Texttable
 
+
+#
+#   使用 |A&B|/sqrt(|A || B |)计算余弦距离
+#
+#
+#
+def calcCosDistSpe(user1,user2):
+    avg_x=0.0
+    avg_y=0.0
+    for key in user1:
+        avg_x+=key[1]
+    avg_x=avg_x/len(user1)
+    
+    for key in user2:
+        avg_y+=key[1]
+    avg_y=avg_y/len(user2)
+    
+    u1_u2=0.0
+    for key1 in user1:
+        for key2 in user2:
+            if key1[1] > avg_x and key2[1]>avg_y and key1[0]==key2[0]:
+                u1_u2+=1
+    u1u2=len(user1)*len(user2)*1.0
+    sx_sy=u1_u2/math.sqrt(u1u2)
+    return sx_sy
+
+
 #
 #   计算余弦距离
 #
@@ -117,7 +144,7 @@ def createUserRankDic(rates):
 #
 def calcNearestNeighbor(userid,users_dic,item_dic):
     neighbors=[]
-    neighbors.append(userid)
+    #neighbors.append(userid)
     for item in users_dic[userid]:
         for neighbor in item_dic[item[0]]:
             if neighbor != userid and neighbor not in neighbors: 
@@ -125,9 +152,10 @@ def calcNearestNeighbor(userid,users_dic,item_dic):
       
     neighbors_dist=[]
     for neighbor in neighbors:
-        dist=calcCosDist(users_dic[userid],users_dic[neighbor])  #calcSimlaryCosDist  calcCosDist
+        dist=calcSimlaryCosDist(users_dic[userid],users_dic[neighbor])  #calcSimlaryCosDist  calcCosDist calcCosDistSpe
         neighbors_dist.append([dist,neighbor])
     neighbors_dist.sort(reverse=True)
+    #print neighbors_dist
     return  neighbors_dist
 
 
@@ -151,18 +179,31 @@ def recommendByUserFC(file_name,userid,k=5):
     
     #寻找邻居
     neighbors=calcNearestNeighbor(userid,test_dic,test_item_to_user)[:k]
-    
-    #建立推荐列表
-    recommend_list=[]
-    user_movies = [ i[0] for i in test_dic[userid]]
+        
+    recommend_dic={}
     for neighbor in neighbors:
         neighbor_user_id=neighbor[1]
         movies=test_dic[neighbor_user_id]
         for movie in movies:
-            if movie[0] not in recommend_list and movie[0] not in user_movies: 
-                recommend_list.append(movie[0])
+            #print movie
+            if movie[0] not in recommend_dic:
+                recommend_dic[movie[0]]=neighbor[0]
+            else:
+                recommend_dic[movie[0]]+=neighbor[0]
+    #print len(recommend_dic)
     
-    return recommend_list,user_movies,test_item_to_user,neighbors
+    #建立推荐列表
+    recommend_list=[]
+    for key in recommend_dic:
+        #print key
+        recommend_list.append([recommend_dic[key],key])
+    
+    
+    recommend_list.sort(reverse=True)
+    #print recommend_list
+    user_movies = [ i[0] for i in test_dic[userid]]
+
+    return [i[1] for i in recommend_list],user_movies,test_item_to_user,neighbors
     
     
 
@@ -189,7 +230,7 @@ if __name__ == '__main__':
     reload(sys)
     sys.setdefaultencoding('utf-8')
     movies=getMoviesList("/Users/wuyinghao/Downloads/ml-100k/u.item")
-    recommend_list,user_movie,items_movie,neighbors=recommendByUserFC("/Users/wuyinghao/Downloads/ml-100k/u.data",293,20)
+    recommend_list,user_movie,items_movie,neighbors=recommendByUserFC("/Users/wuyinghao/Downloads/ml-100k/u.data",24,80)
     neighbors_id=[ i[1] for i in neighbors]
     table = Texttable()
     table.set_deco(Texttable.HEADER)
@@ -199,7 +240,7 @@ if __name__ == '__main__':
     table.set_cols_align(["l", "l", "l"])
     rows=[]
     rows.append([u"movie name",u"release", u"from userid"])
-    for movie_id in recommend_list[:10]:
+    for movie_id in recommend_list[:20]:
         from_user=[]
         for user_id in items_movie[movie_id]:
             if user_id in neighbors_id:
@@ -207,8 +248,8 @@ if __name__ == '__main__':
         rows.append([movies[movie_id][0],movies[movie_id][1],from_user])
     table.add_rows(rows)
     print table.draw()
-    print neighbors
-    print user_movie
+    #print neighbors
+    #print user_movie
     #print user_movie
     #print neighbors
 
