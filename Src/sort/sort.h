0948d544aa84be45d28633580559c9fb2d300418
Wu Yinghao <wyh817@gmail.com>
2013-11-19 11:21:10 +0800

    add quicksort
    add quicksort

--- Src/sort/sort.h
+++ Src/sort/sort.h
@@ -36,8 +36,8 @@ namespace aglo{
 				temp=(*m_sort_list)[i];
 				j=i-1;
 				while((j>=0) && (compre((*m_sort_list)[j],temp)==true)){
-				     (*m_sort_list)[j+1] = (*m_sort_list)[j];
-				     j--;
+					(*m_sort_list)[j+1] = (*m_sort_list)[j];
+					j--;
 				}
 				(*m_sort_list)[j+1] = temp;
 				/* code */
@@ -73,7 +73,7 @@ namespace aglo{
 		//	快速排序
 		//
 		void quickSort(){
-			
+			_quickSort((*m_sort_list),0,(*m_sort_list).size());
 		}
 		
 		
@@ -88,9 +88,54 @@ namespace aglo{
 			//cout << compre(4,3) << endl;
 		}
 	
+	
+	
+	
 	private:
 		vector<T> *m_sort_list;
 		bool (*compre)(T,T);
+		
+		//
+		//	快速排序具体实现
+		//
+		void _quickSort(vector<T>& arr,int start,int end){
+			
+			int i,j;
+			i = start;
+			j = end;
+			if((arr.size()==0))
+			     return;
+			         
+			while(i<j){
+			      
+				while(i<j && compre(arr[j],arr[i])){     
+					j--;
+				}
+				if(i<j){                  
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+				   
+				   
+				while(i<j&&compre(arr[j],arr[i])){    
+					i++;
+				}
+				if(i<j){                 
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+					
+			if(i-start>1){
+				_quickSort(arr,start,i-1);
+			}
+					
+			if(end-j>1){
+				_quickSort(arr,j+1,end);
+			}
+		}
 
 	
 	};
@@ -104,7 +149,7 @@ namespace aglo{
 
 
 bool comp(int a,int b){
-	if (a>b)
+	if (a>=b)
 		return true;
 	//cout << a << endl;
 	return false;
@@ -116,7 +161,7 @@ void makeArray(vector<int>& array,int num){
 		
 	for(size_t i = 0; i < num; ++i)
 	{
-		array.push_back(rand()%100);
+		array.push_back(rand()%1000);
 		/* code */
 	}
 	
@@ -126,11 +171,19 @@ void makeArray(vector<int>& array,int num){
 int main (int argc, char const *argv[])
 {
 	vector<int> a;
-	makeArray(a,10);
 	//cout << rand()%100 << endl;
 	//cout << rand()%100 << endl;
 	aglo::SortClass<int> sort(a,comp);
-	
+	makeArray(a,50);
+	cout << "############### 快速排序法 ###############" << endl;
+	cout << "排序前::: " ;
+	sort.displaySort();
+	sort.quickSort();
+	cout << "排序后::: ";
+	sort.displaySort();
+	cout << endl;
+	a.clear();
+	makeArray(a,50);
 	cout << "############### 插入排序法 ###############" << endl;
 	cout << "排序前::: " ;
 	sort.displaySort();
@@ -139,13 +192,14 @@ int main (int argc, char const *argv[])
 	sort.displaySort();
 	cout << endl;
 	a.clear();
-	makeArray(a,10);
+	makeArray(a,50);
 	cout << "############### 冒泡排序法 ###############" << endl;
 	cout << "排序前::: " ;
 	sort.displaySort();
 	sort.bubbleSort();
 	cout << "排序后::: ";
 	sort.displaySort();
+	
 	
 	
 	
